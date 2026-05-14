30d0ffedbfc7e9c00cf03b9aad5350770733b819
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-14 20:24:07 +0600

    create full sever to node js

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
 const server: Server = createServer(
-  (req: IncomingMessage, res: ServerResponse) => {},
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log(req);
+  },
 );
+
+server.listen(3000, () => {
+  console.log("server is running 3000");
+});
