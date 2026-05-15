c643aec9b91a31aa2846942207750a95cb63d71b
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-15 23:40:54 +0600

    product rout create

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ const server: Server = createServer(
     if (url === "/" && method === "GET") {
       res.writeHead(200, { "content-type": "application/json" });
       res.end(JSON.stringify({ message: "this is route route" }));
+    } else if (url?.startsWith("/product")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "product" }));
     } else {
       res.writeHead(404, { "content-type": "application/json" });
       res.end(JSON.stringify({ message: "route not found" }));
