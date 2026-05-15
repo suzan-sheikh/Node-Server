d339f148b4ed7316bc0d060c660d6a9e31c3c53b
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-15 17:48:51 +0600

    create a server

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
+
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log(req);
+  },
+);
+
+server.listen(3000, () => {
+  console.log("server is working on 3000");
+});
